ba3e46fb6d15cac7e83e77650e6de314bd32f671
Austin Rainwater <paco@heckin.io>
2022-03-26 14:55:34 -0400

    Create an Alfred Python shim

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+set -e
+
+# Default to python3
+if [[ -z $PYTHON ]]; then
+    PYTHON=python3
+fi
+
+# Make sure pip is set up
+${PYTHON} -m ensurepip
+
+# Create a temporary directory for virtualenv
+mkdir -p tmp
+
+# Use pip to install virtualenv
+${PYTHON} -m pip install -t tmp virtualenv
+
+# Use virtualenv to create a virtual environment
+${PYTHON} -m tmp.virtualenv venv
+
+# Delete the temp directory
+rm -rf tmp
+
+# Use virtual environment pip to upgrade itself
+venv/bin/python -m pip install --upgrade pip
+
+# Use virtual environment pip to install requirements
+venv/bin/python -m pip install -r requirements.txt
